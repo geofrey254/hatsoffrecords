0edcb2cead06b974759b07f6f9131862fbdf2ebb
geofrey254 <devongeoffreymaina@gmail.com>
2022-01-18 11:29:35 +0300

    Debug set to true due to server error heroku

--- hatsoff/settings.py
+++ hatsoff/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(f_*mwwxezlw474uwd^*ezb+v6e&!rpu+b(5=cg1@(^zi80det'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hatsoffrec.herokuapp.com', 'localhost']
 
